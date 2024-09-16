2e3d46c40f4bea777c17c9736cef2294bc574794
Serani <delvinlim6414@gmail.com>
2024-09-16 08:01:49 +0800

    Change from having individual date and time view models to a combined datetime viewmodel

--- app/src/main/java/com/example/mind_care/home/reminders/adapter/CreateReminderAlertItemAdapter.java
+++ app/src/main/java/com/example/mind_care/home/reminders/adapter/CreateReminderAlertItemAdapter.java
@@ -7,20 +7,19 @@ import android.widget.Button;
 import android.widget.DatePicker;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.MutableLiveData;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mind_care.R;
 import com.example.mind_care.home.reminders.fragment.DatePickerFragment;
-import com.example.mind_care.home.reminders.model.ReminderAlertItem;
 import com.example.mind_care.home.reminders.viewModel.ReminderAlertDateViewModel;
 import com.example.mind_care.home.reminders.viewModel.ReminderAlertTimeViewModel;
 
-import java.util.List;
-
-public class CreateReminderAlertItemAdapter extends RecyclerView.Adapter<CreateReminderAlertItemAdapter.ReminderItemViewHolder> {
+public class CreateReminderAlertItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     ReminderAlertDateViewModel alertDateViewModel;
     ReminderAlertTimeViewModel alertTimeViewModel;
+
+    private final int ITEM_VIEW_TYPE_ADD_NEW = 0;
+    private final int ITEM_VIEW_TYPE_REMINDER = 1;
 
     public CreateReminderAlertItemAdapter(ReminderAlertDateViewModel alertDateViewModel, ReminderAlertTimeViewModel alertTimeViewModel) {
         this.alertDateViewModel = alertDateViewModel;
@@ -29,15 +28,32 @@ public class CreateReminderAlertItemAdapter extends RecyclerView.Adapter<CreateR
 
     @NonNull
     @Override
-    public ReminderItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.reminders_create_reminder_alert_item, parent, false);
-        return new ReminderItemViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == ITEM_VIEW_TYPE_ADD_NEW) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.reminders_create_reminder_add_new_alert_item, parent, false);
+            return new ReminderAddItemViewHolder(view);
+        }
+        else{
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.reminders_create_reminder_alert_item, parent, false);
+            return new ReminderItemViewHolder(view);
+        }
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ReminderItemViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         //TODO
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount()){
+            return ITEM_VIEW_TYPE_ADD_NEW;
+        }
+        else {return ITEM_VIEW_TYPE_REMINDER;}
+    }
+
 
     @Override
     public int getItemCount() {
@@ -50,7 +66,7 @@ public class CreateReminderAlertItemAdapter extends RecyclerView.Adapter<CreateR
 
         public ReminderItemViewHolder(@NonNull View itemView) {
             super(itemView);
-            reminderItem = itemView.findViewById(R.id.create_reminder_item_button);
+            reminderItem = itemView.findViewById(R.id.alert_item_button);
             deleteButton = itemView.findViewById(R.id.alert_item_delete_button);
 
             int position = getAdapterPosition();
@@ -60,14 +76,36 @@ public class CreateReminderAlertItemAdapter extends RecyclerView.Adapter<CreateR
                     DatePickerFragment datePickerFragment = new DatePickerFragment() {
                         @Override
                         public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-                            alertDateViewModel.addDateToArray(dayOfMonth, month, year);
+                            //TODO
                         }
                     };
-                    datePickerFragment.show(view.getContext().getSupportFragmentManager(), "date picker");
                 }
             });
         }
+    }
 
+    public class ReminderAddItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private Button reminderItem;
+        private Button addButton;
 
+        public ReminderAddItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            reminderItem = itemView.findViewById(R.id.alert_item_add_button2);
+            addButton = itemView.findViewById(R.id.alert_item_add_button);
+
+            int position = getAdapterPosition();
+            addButton.setOnClickListener(this);
+            reminderItem.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            DatePickerFragment datePickerFragment = new DatePickerFragment() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+
+                }
+            };
+        }
     }
 }
